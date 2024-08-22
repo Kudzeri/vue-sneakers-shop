39a1de3d2c68f63abc993f0ac31c18ba70b5f4bd
Kudzeri <kudzeri@gmail.com>
2024-08-22 08:54:35 +0500

    add: Header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue';
+</script>
 
 <template>
+  <div class="bg-white rounded-xl shadow-xl w-4/5 m-auto mt-14 h-screen">
+    <Header />
+    
+
+  </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="flex justify-between px-10 py-8 border-b border-slate-300">
+    <div class="flex items-center gap-4">
+      <img class="w-10" src="/logo.png" alt="Logo" />
+      <div>
+        <h2 class="font-bold text-xl uppercase">Vue scneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 text-gray-400 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="Cart" />
+        <span>1205 руб.</span>
+      </li>
+      <li class="flex items-center gap-3 text-gray-400 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="Heart" />
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center gap-3 text-gray-400 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="Profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
