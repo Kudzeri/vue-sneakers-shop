a1aaba4bb8ebd643c7fdbf87495e079113c7c524
Kudzeri <kudzeri@gmail.com>
2024-08-22 12:53:24 +0500

    init: Drawer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
+  <Drawer />
   <div class="bg-white rounded-xl shadow-xl w-4/5 m-auto mt-14">
     <Header />
 
