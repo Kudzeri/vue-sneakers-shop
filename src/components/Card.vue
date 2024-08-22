2a8c0837a0e00c094ed787173a534f70a4ab3753
Kudzeri <kudzeri@gmail.com>
2024-08-22 10:35:40 +0500

    upd: added dynamism for component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,23 @@
+<script setup>
+defineProps({
+  imageUrl: String,
+  title: String,
+  cost: Number
+})
+</script>
+
 <template>
   <div
-    class="relative m-10 p-8 border border-slate-100 rounded-3xl cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+    class="relative p-8 bg-white border border-slate-100 rounded-3xl cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
     <img class="absolute top-8 left-8" src="/like-2.svg" alt="Like" />
-    <img src="/sneakers/sneakers-1.jpg" alt="Sneakers img" />
-    <p>Мужские кроссовки Nike 1</p>
+    <img :src="imageUrl" alt="Sneakers img" />
+    <p>{{ title }}</p>
 
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400">Цена:</span>
-        <span class="font-bold">12100 тг.</span>
+        <span class="font-bold">{{ cost }} тг.</span>
       </div>
 
       <img src="/plus.svg" alt="Plus" />
