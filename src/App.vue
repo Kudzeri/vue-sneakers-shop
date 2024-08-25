3f13b6b796a8d99a6ac2e60041058ffcbeda3fe1
Kudzeri <kudzeri@gmail.com>
2024-08-25 19:45:33 +0500

    add:Loading backend data on the main page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://cfd9563998ee0fec.mokky.dev/items')
+
+    items.value = data
+  } catch (e) {
+    console.log(e)
+  }
+})
 </script>
 
 <template>
@@ -15,22 +30,25 @@ import Drawer from './components/Drawer.vue'
 
         <div class="flex gap-4">
           <select class="py-2 px-3 border rounded-md outline-none">
-          <option >По названию</option>
-          <option >По цене (дешевые)</option>
-          <option >По цене (дорогие)</option>
-        </select>
+            <option>По названию</option>
+            <option>По цене (дешевые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
 
-        <div class="relative cursor-pointer">
-          <img class="absolute left-4 top-3" src="/search.svg" alt="Search">
-          <input 
-            class="border border-slate-200 py-2 pl-12 pr-4 rounded-lg outline-none focus:border-gray-400" 
-            placeholder="Поиск..." 
-            type="text">
-        </div>
+          <div class="relative cursor-pointer">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="Search" />
+            <input
+              class="border border-slate-200 py-2 pl-12 pr-4 rounded-lg outline-none focus:border-gray-400"
+              placeholder="Поиск..."
+              type="text"
+            />
+          </div>
         </div>
       </div>
 
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,10 @@
 <script setup>
 import Card from './Card.vue'
 
+defineProps({
+  items: Array
+})
+
 const onClickAdd = () => {
   alert('added')
 }
@@ -10,15 +14,17 @@ const onClickFavorite = () => {
 </script>
 
 <template>
-    <div class="grid grid-cols-4 gap-5">
-      <Card
-        image-url="/sneakers/sneakers-1.jpg"
-        title="Кроссовки Nike 1"
-        :cost="12200"
-        :is-added="true"
-        :is-favorite="true"
-        :on-click-add="onClickAdd"
-        :on-click-favorite="onClickFavorite"
-      />
-    </div>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :image-url="item.imageUrl"
+      :title="item.title"
+      :cost="item.price"
+      :is-added="false"
+      :is-favorite="false"
+      :on-click-add="onClickAdd"
+      :on-click-favorite="onClickFavorite"
+    />
+  </div>
 </template>
